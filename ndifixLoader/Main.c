56f5f16b2cdfda83444dd41112f8adc2070735fd
ndifix <yoshiki.linuxmint@gmail.com>
2021-03-22 20:26:40 +0900

    EDK 2 を使ってLoaderを作成するようにした

---
+++ ndifixLoader/Main.c
@@ -0,0 +1,11 @@
+#include <Library/UefiLib.h>
+
+// エントリーポイント
+EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
+                           EFI_SYSTEM_TABLE* system_table) {
+  Print(L"Hello ndifix os\n");
+  while (1)
+    ;
+
+  return EFI_SUCCESS;
+}
