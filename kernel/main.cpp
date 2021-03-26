997ed0b9c9146a5b0a5046e90fda31a4d7846ab7
ndifix <yoshiki.linuxmint@gmail.com>
2021-03-27 01:34:20 +0900

    kernel: 画面描画のクラスを定義した

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,15 +1,9 @@
 #include <cstdint>
 
 #include "frame_buffer_config.hpp"
-
-void PrintScreen(const ndifixos_FrameBuffer::FrameBufferConfig& config) {
-  for (uint64_t i = 0; i < config.frame_buffer_size; ++i) {
-    config.frame_buffer_base[i] = i % 0x100;
-  }
-}
+#include "graphics.hpp"
 
 extern "C" void KernelMain(
     const ndifixos_FrameBuffer::FrameBufferConfig& config) {
-  PrintScreen(config);
   while (1) __asm__("hlt");
 }
