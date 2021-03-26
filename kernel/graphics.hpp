0e5268e6aa3dbb5732eb7c733842bad8706678f0
ndifix <yoshiki.linuxmint@gmail.com>
2021-03-27 01:34:20 +0900

    kernel: 画面描画のクラスを定義した

---
+++ kernel/graphics.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <cstdint>
+
+#include "frame_buffer_config.hpp"
+
+namespace ndifixos_graphics {
+
+struct PixelColor {
+  uint8_t R, G, B;
+};
+
+// ピクセル毎に描画を管理します
+class PixelWriter {
+ private:
+  const ndifixos_FrameBuffer::FrameBufferConfig config;
+
+ protected:
+  uint8_t* PixelAt(int x, int y) {
+    return config.frame_buffer_base + 4 * (config.pixels_per_scan_line * y + x);
+  }
+
+ public:
+  PixelWriter(const ndifixos_FrameBuffer::FrameBufferConfig& c) : config(c) {}
+  ~PixelWriter() = default;
+
+  // 指定された1ピクセルを指定した色で描画します
+  void Write(int x, int y, const PixelColor& c) {
+    auto p = PixelAt(x, y);
+    switch (config.pixel_format) {
+      case ndifixos_FrameBuffer::kPixelRGBResv8BitPerColor:
+        p[0] = c.R;
+        p[1] = c.G;
+        p[2] = c.B;
+        break;
+      case ndifixos_FrameBuffer::kPixelBGResv8BitPerColor:
+        p[0] = c.B;
+        p[1] = c.G;
+        p[2] = c.R;
+        break;
+    }
+  }
+};
+
+}  // namespace ndifixos_graphics
--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,12 +1,7 @@
 #include <cstdint>
 
 #include "frame_buffer_config.hpp"
-
-void PrintScreen(const ndifixos_FrameBuffer::FrameBufferConfig& config) {
-  for (uint64_t i = 0; i < config.frame_buffer_size; ++i) {
-    config.frame_buffer_base[i] = i % 0x100;
-  }
-}
+#include "graphics.hpp"
 
 extern "C" void KernelMain(
     const ndifixos_FrameBuffer::FrameBufferConfig& config) {
