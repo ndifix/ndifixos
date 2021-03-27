09daee7fbe97fbd6af22edc992685e1bf1a7ce88
ndifix <yoshiki.linuxmint@gmail.com>
2021-03-27 21:33:40 +0900

    console: コンソール出力ができるようにした

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,9 +1,13 @@
 #include <cstdint>
 
+#include "console.hpp"
 #include "frame_buffer_config.hpp"
 #include "graphics.hpp"
 
 extern "C" void KernelMain(
     const ndifixos_FrameBuffer::FrameBufferConfig& config) {
+  const ndifixos_graphics::PixelWriter writer(config);
+  ndifixos_console::Console console(writer);
+
   while (1) __asm__("hlt");
 }
