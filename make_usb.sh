31e03758db748826ba73be31663f196a9ab18f44
ndifix <yoshiki.linuxmint@gmail.com>
2021-03-27 19:49:08 +0900

    modified Makefile

---
+++ make_usb.sh
@@ -0,0 +1,9 @@
+cd ~/ndifixos/
+source build.sh
+sudo mkdir -p /mnt/usbmem
+sudo mount -t drvfs D: /mnt/usbmem
+sudo mkdir -p /mnt/usbmem/EFI/BOOT
+sudo cp ~/edk2/Build/ndifixLoaderX64/DEBUG_CLANG38/X64/Loader.efi /mnt/usbmem/EFI/BOOT/BOOTX64.EFI
+sudo cp ~/ndifixos/kernel/kernel.elf /mnt/usbmem
+sudo umount /mnt/usbmem
+
