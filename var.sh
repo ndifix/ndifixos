b39f7cd8a97494110b97eaebd98d6984fa1f1202
ndifix <yoshiki.linuxmint@gmail.com>
2022-02-05 14:39:38 +0900

    bash ファイルのsource を消した

--- var.sh
+++ var.sh
@@ -1,4 +1,4 @@
-# Usage: source buildenv.sh
+# Usage: ./build.sh
 
 BASEDIR="$HOME/osbook/devenv/x86_64-elf"
 export CPPFLAGS="-I$BASEDIR/include/c++/v1 -I$BASEDIR/include -I$BASEDIR/include/freetype2 -nostdlibinc -D__ELF__ -D_LDBL_EQ_DBL -D_GNU_SOURCE -D_POSIX_TIMERS"
