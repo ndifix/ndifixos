f48e07c79252806184a97e28251720d676b5a8a1
ndifix <yoshiki.linuxmint@gmail.com>
2022-02-05 17:28:13 +0900

    library を追加

--- devenv/var.sh
+++ devenv/var.sh
@@ -7,6 +7,6 @@ export DSCFILE=${ROOTDIR}/ndifixLoader/ndifixLoaderPkg.dsc
 export EDK_OUT=${EDKDIR}/Build/ndifixLoaderX64/DEBUG_CLANG38/X64/Loader.efi
 export KERNEL_OUT=${KERNEL}/kernel.elf
 
-BASEDIR="$HOME/osbook/devenv/x86_64-elf"
+BASEDIR=${DEVENV}/x86_64-elf
 export CPPFLAGS="-I$BASEDIR/include/c++/v1 -I$BASEDIR/include -I$BASEDIR/include/freetype2 -nostdlibinc -D__ELF__ -D_LDBL_EQ_DBL -D_GNU_SOURCE -D_POSIX_TIMERS"
 export LDFLAGS="-L$BASEDIR/lib"
